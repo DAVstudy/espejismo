c97b229b4eb6c9ad23c5c3802acba7e109f4c55a
DIEGO ARENAS <arenasvega.diego@gmail.com>
2024-11-24 13:08:34 -0300

    change config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://DAVstudy.github.io',
-  base: '/espejismo',
-  trailingSlash: "always"
+  site: 'https://DAVstudy.github.io'
 });
